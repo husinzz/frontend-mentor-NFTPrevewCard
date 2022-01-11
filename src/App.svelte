cc440a89c478384c8a7fc7bf01e35969bea7758c
husinzz <its.husin@gmail.com>
2022-01-11 20:03:53 +0700

    intial commit, challenge complete.

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,80 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  let hidden: string = "hidden";
+  function hide(e) {
+    hidden = "hidden";
+  }
+
+  function unHide(e) {
+    hidden = "";
+  }
 </script>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
-  <h2 class="text-blue-500 text-4xl">The template works!</h2>
+<main
+  class="bg-blue-very-dark-1 flex justify-center items-center h-full font-outfits"
+>
+  <div class="bg-blue-very-dark-2 rounded-lg max-w-[350px]">
+    <div class="relative m-5 rounded-lg overflow-hidden">
+      <img
+        on:mouseover={unHide}
+        on:mouseout={hide}
+        src="images/image-equilibrium.jpg"
+        class="h-full"
+        alt="NFT"
+      />
+      <div
+        class={`h-full w-full bg-cyan-500 bg-opacity-75 absolute top-0 flex items-center justify-center ${hidden}`}
+      >
+        <img src="images/icon-view.svg" alt="eth" class="opacity-100 z-10" />
+      </div>
+    </div>
+    <div class="mx-5">
+      <h1 class="text-white text-2xl font-semibold pb-3 hover:text-cyan-500">
+        Equilibrium #3429
+      </h1>
+      <p class="text-blue-soft text-lg font-light pb-3">
+        Our Equilibrium collection promotes balance and calm.
+      </p>
+      <div class="flex items-center justify-between pb-5">
+        <span class="flex items-center"
+          ><img src="images/icon-ethereum.svg" alt="eth" class="h-[18px]" />
+          <p class="text-cyan-500 font-semibold pl-2">0.041 ETH</p></span
+        >
 
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+        <span class="flex items-center"
+          ><img src="images/icon-clock.svg" alt="time-left" class="h-[18px]" />
+          <p class="text-blue-soft pl-2">3 days left</p></span
+        >
+      </div>
+      <hr class="py-2 very-dark-3" />
+      <div class="flex items-center pb-5">
+        <img
+          src="images/image-avatar.png"
+          alt="avatar"
+          class="w-[40px] ml-2 mr-4 border-white border rounded-full"
+        />
+        <p class="text-blue-soft">
+          Creation of <span class="text-white hover:text-cyan-500"
+            >Jules Wyvern</span
+          >
+        </p>
+      </div>
+    </div>
+  </div>
 </main>
 
+<div class="attribution bottom-0 left-1/2 -translate-x-1/2 absolute my-2 text-white">
+  Challenge by <a
+    href="https://www.frontendmentor.io?ref=challenge"
+    >Frontend Mentor</a
+  >. Coded by <a href="https://github.com/urdreamboi">urdreamboi</a>.
+</div>
+
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
+  .attribution {
+    font-size: 11px;
     text-align: center;
-    padding: 1em;
-    margin: 0 auto;
   }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
+  .attribution a {
+    color: hsl(228, 5%, 62%);
   }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@ const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue : {
+          "soft" : "hsl(215, 51%, 70%)",
+          "very-dark-1" : "hsl(217, 54%, 11%)",
+          "very-dark-2" : "hsl(216, 50%, 16%)",
+          "very-dark-3" : "hsl(215, 32%, 27%)",
+        }
+      }
+    },
+    fontFamily : {
+      'outfits' : ['Outfit', 'ui-sans-serif']
+    }
   },
 
   plugins: [],
